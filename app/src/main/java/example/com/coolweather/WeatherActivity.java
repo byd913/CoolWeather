329c7ee0d3f18656b041c12c4256d99229ff21dd
baiyoudong <baiyoudong@didichuxing.com>
2017-04-07 16:03:16 +0800

    fix some show

--- app/src/main/java/example/com/coolweather/WeatherActivity.java
+++ app/src/main/java/example/com/coolweather/WeatherActivity.java
@@ -1,7 +1,9 @@
 package example.com.coolweather;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.media.Image;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -56,6 +58,13 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_weather);
 
         weatherLayout = (ScrollView)findViewById(R.id.weather_layout);
